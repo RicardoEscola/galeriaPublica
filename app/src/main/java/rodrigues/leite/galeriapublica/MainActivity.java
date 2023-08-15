ad2537d66180af46375d400dc3ba53ce99454fb1
20211TIIMI0265 <ricardoleiteescola@gmail.com>
2023-08-15 10:36:51 -0300

    Antes que de merda

--- app/src/main/java/rodrigues/leite/galeriapublica/MainActivity.java
+++ app/src/main/java/rodrigues/leite/galeriapublica/MainActivity.java
@@ -2,17 +2,41 @@ package rodrigues.leite.galeriapublica;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
-import android.app.Application;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+        bottomNavigationView = findViewById(R.id.btNav);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                vm.setNavigationOpSelect(item.getItemId());
+                switch (item.getItemId()){
+                    case R.id.listViewOp:
+                        GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                        setFragment(gridViewFragment);
+                        break;
+                    case R.id.listViewOp:
+                        ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                        setFragment(listViewFragment);
+                        break;
+                }
+                return true;
+        }
+    });
     }
+
 }
 
