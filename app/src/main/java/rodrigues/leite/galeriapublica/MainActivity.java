45a8fb2c004c00ab739affaa6c83535daacc98d7
20211TIIMI0265 <ricardoleiteescola@gmail.com>
2023-08-15 10:50:41 -0300

    Passo 7

--- app/src/main/java/rodrigues/leite/galeriapublica/MainActivity.java
+++ app/src/main/java/rodrigues/leite/galeriapublica/MainActivity.java
@@ -2,6 +2,8 @@ package rodrigues.leite.galeriapublica;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -11,18 +13,32 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView; // Criação dessa variavel
+
+    void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Referencia para o View Model
         final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+
+        // Referencia para o BottomViewNav
         bottomNavigationView = findViewById(R.id.btNav);
+        // Escutador de clique
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                //Pegamos a opção que o usuario escolheu
                 vm.setNavigationOpSelect(item.getItemId());
+
+                //caso o usuario selecione a grid vai pra la e o retorno tbm
                 switch (item.getItemId()){
                     case R.id.listViewOp:
                         GridViewFragment gridViewFragment = GridViewFragment.newInstance();
